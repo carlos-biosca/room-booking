25c925b8c4ad3645f2cb1a200a955973b465c2dc
carlos-biosca <cbiosca82@gmail.com>
2024-10-08 22:58:37 +0200

    Header and react-icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,20 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Book Room App",
-  description: "Register and book a meeting room for your team ",
+  description: "Register and book a meeting room for your team "
 };
 
-export default function RootLayout ({ children }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
+      <body className={inter.className}>
+        <Header />
         <main className="mx-auto max-w-7xl min-h-screen px-4 py-6 bg-indigo-500 sm:px-6 lg:px-8">
           {children}
         </main>
