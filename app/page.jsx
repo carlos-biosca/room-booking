ff166c0e5351c7a7d1d213d0b7689a556b2e8672
carlos-biosca <cbiosca82@gmail.com>
2024-10-10 12:34:41 +0200

    room card

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,14 @@
-export default function Home () {
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+
+export default function Home() {
   return (
     <>
-      <h1>rooms</h1>
+      {rooms.length > 0 ? (
+        rooms.map(room => <RoomCard key={room.$id} room={room} />)
+      ) : (
+        <h1>no rooms</h1>
+      )}
     </>
   );
 }
---
+++ components/RoomCard.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const RoomCard = ({ room }) => {
+  return (
+    <div className="bg-white mb-14">
+      <div className="flex flex-col sm:flex-row sm:items-center">
+        <div className="relative h-[250px] sm:h-[320px] sm:flex-1">
+          <Image
+            src={`/images/${room.image}`}
+            alt={room.name}
+            className="object-cover rounded-lg sm:rounded-[20px] sm:shadow"
+            fill
+          />
+        </div>
+        <div className="sm:space-y-2 sm:w-1/2 lg:w-1/3 sm:px-6">
+          <div className="p-4 sm:p-0">
+            <h4 className="text-xl sm:text-2xl font-bold">{room.name}</h4>
+            <p className="sm:text-lg text-gray-600">
+              <span className="font-semibold text-gray-800"> Address: </span>
+              {room.address}
+            </p>
+            <p className="sm:text-lg text-gray-600">
+              <span className="font-semibold text-gray-800">
+                Availability:{" "}
+              </span>
+              {room.availability}
+            </p>
+            <p className="sm:text-lg text-gray-600">
+              <span className="font-semibold text-gray-800"> Price: </span>$
+              {room.price_per_hour}/hour
+            </p>
+          </div>
+          <Link
+            href={`/rooms/${room.$id}`}
+            className="block sm:inline-block bg-gray-700 text-white px-4 py-3 sm:py-2 rounded w-full sm:w-auto text-center hover:bg-gray-500"
+          >
+            View Room
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RoomCard;
