d7af7458f9c953eff00b0f5004d9c5538a557aa8
carlos-biosca <cbiosca82@gmail.com>
2024-10-18 11:29:36 +0200

    login session action

--- app/actions/createSession.js
+++ app/actions/createSession.js
@@ -1,15 +1,30 @@
 'use server'
 
+import { createAdminClient } from "@/config/appwrite"
+import { cookies } from "next/headers"
+
 async function createSession (previousState, formData) {
   const email = formData.get('email')
   const password = formData.get('password')
 
-  if (!email || !password) {
+  const { account } = await createAdminClient()
+
+  try {
+    const session = await account.createEmailPasswordSession(email, password)
+    cookies().set('app-session', {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      expires: new Date(session.expire),
+      path: '/'
+    })
+    return { success: true }
+  } catch (err) {
+    console.log(err);
     return {
-      error: 'Please fill out all fields'
+      error: 'Invalid email or password'
     }
   }
-  return { success: true }
 }
 
 export default createSession;
