4baf61b1023382db9785382408bb33dd6749f192
Carlos Biosca <39582461+carlos-biosca@users.noreply.github.com>
2025-02-24 20:53:09 +0100

    Update HomeGallery.jsx

--- components/HomeGallery.jsx
+++ components/HomeGallery.jsx
@@ -9,25 +9,25 @@ const HomeGallery = () => {
   return (
     <section className="h-auto md:h-[40vh] xl:h-[60vh] grid grid-cols-4 grid-rows-2 gap-5">
       <Image
-        className="h-full w-auto col-span-4 md:col-span-3 lg:col-span-2 row-span-2 object-cover rounded-lg md:rounded-3xl"
+        className="h-full w-full col-span-4 md:col-span-3 lg:col-span-2 row-span-2 object-cover rounded-lg md:rounded-3xl"
         src={gallery1}
         alt="image gallery 1"
         priority
       />
       <Image
-        className="h-full w-auto hidden lg:block col-span-1 row-span-2 object-cover rounded-3xl"
+        className="h-full w-full hidden lg:block col-span-1 row-span-2 object-cover rounded-3xl"
         src={gallery2}
         alt="image gallery 2"
         priority
       />
       <Image
-        className="h-full w-auto hidden md:block col-span-1 row-span-1 object-cover rounded-3xl"
+        className="h-full w-full hidden md:block col-span-1 row-span-1 object-cover rounded-3xl"
         src={gallery3}
         alt="image gallery 3"
         priority
       />
       <Image
-        className="h-full w-auto hidden md:block col-span-1 row-span-1 object-cover rounded-3xl"
+        className="h-full w-full hidden md:block col-span-1 row-span-1 object-cover rounded-3xl"
         src={gallery4}
         alt="image gallery 4"
         priority
